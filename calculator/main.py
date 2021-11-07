a1037ecd3fd0675a991b3d2b61d742a21b67efd8
BobSchicke <bobschicke@gmail.com>
2021-11-07 17:36:03 -0500

    Modified tests and calculator functions. Added divide function and 2 divide tests

--- calculator/main.py
+++ calculator/main.py
@@ -1,21 +1,58 @@
-""" This is the increment function"""
+""" This is Main.py """
 class Calculator:
     """ This is the Calculator class"""
 
+    # The input function will will put the first value in the object's result variable
+    # This allows the program to only have to pass one value
+    # (see commented main function below)
+
+    # result is a class variable
     result = 0
+
     def get_result(self):
         """ Get Result of Calculation"""
         return self.result
 
-    def add_number(self, value_a):
-        """ adds number to result"""
-        self.result = self.result + value_a
+    def add_number(self, value_b):
+        """ adds number to result """
+        self.result = self.result + value_b
         return self.result
-    def subtract_number(self, value_a):
-        """ subtract number from result"""
-        self.result = self.result - value_a
+
+    def subtract_number(self, value_b):
+        """ subtracts number from result"""
+        self.result = self.result - value_b
         return self.result
-    def multiply_numbers(self, value_a, value_b):
-        """ multiply two numbers and store the result"""
-        self.result = value_a * value_b
+
+    def multiply_numbers(self, value_b):
+        """ multiplies two numbers and saves to result """
+        self.result = self.result * value_b
         return self.result
+
+    def divide_numbers(self, value_b):
+        """ divides two numbers and stores the result"""
+        # This catches a divide by 0 error
+        if value_b == 0:
+            return "DivBy0"
+        self.result = self.result / value_b
+        return self.result
+
+#def main():
+# """ This is the Main function """
+# calc = Calculator()
+# leave = "a"
+# while leave not in ("x", "X"):
+#     calc.result = int(input("Enter First Number: "))
+#     num2 = int(input("Enter Second Number: "))
+#     operator = input("Enter an Operator: ")
+#     if operator == '+':
+#         answ = calc.add_number(num2)
+#     if operator == '-':
+#         answ = calc.subtract_number(num2)
+#     if operator == '*':
+#         answ = calc.multiply_numbers(num2)
+#     if operator == '/':
+#         answ = calc.divide_numbers(num2)
+#
+#     print(answ)
+#
+#     leave = input("c to continue: x to exit: ")
--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,32 +1,45 @@
-"""Testing the Calculator"""
+""" These are the tests for the Calculator """
 from calculator.main import Calculator
 
 def test_calculator_result():
-    """testing calculator result is 0"""
+    """ Tests that Calculator's reset variable's initial value is 0 """
     calc = Calculator()
     assert calc.result == 0
 
 def test_calculator_add():
-    """Testing the Add function of the calculator"""
+    """ Tests Addition of 2 numbers"""
     #Arrange by instantiating the calc class
     calc = Calculator()
     #Act by calling the method to be tested
+    calc.result = 5
     calc.add_number(4)
     #Assert that the results are correct
-    assert calc.result == 4
-
-def test_calculator_get_result():
-    """Testing the Get result method of the calculator"""
-    calc = Calculator()
-    assert calc.get_result() == 0
+    assert calc.result == 9
 
 def test_calculator_subtract():
-    """Testing the subtract method of the calculator"""
+    """ Tests Subtraction of 2 numbers """
     calc = Calculator()
-    calc.subtract_number(1)
-    assert calc.get_result() == -1
+    calc.result = 5
+    calc.subtract_number(7)
+    assert calc.get_result() == -2
+
 def test_calculator_multiply():
-    """ tests multiplication of two numbers"""
+    """ Tests Multiplication of two numbers """
     calc = Calculator()
-    result  = calc.multiply_numbers(1,2)
+    calc.result = 3
+    result  = calc.multiply_numbers(2)
+    assert result == 6
+
+def test_calculator_divide():
+    """ Tests Division of two numbers """
+    calc = Calculator()
+    calc.result = 6
+    result  = calc.divide_numbers(3)
     assert result == 2
+
+def test_calculator_divide_by0():
+    """ Tests that Division by Zero catches the error and doesn't crash the program """
+    calc = Calculator()
+    calc.result = 6
+    result  = calc.divide_numbers(0)
+    assert result == "DivBy0"
