018e5f86b11a896da5ce25b39026b495d5903d6e
BobSchicke <bobschicke@gmail.com>
2021-11-13 15:27:29 -0500

    Changed .pylintrc to suppress deprecated Modules, redefined-outer-name, and unused-argument warnings Changed clear_hist() back to pytest.fixture and passed as parameter

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,8 +1,8 @@
 """ These are the tests for the Calculator """
-#import pytest
+import pytest
 from calculator.calculator import Calculator
 
-#@pytest.fixture
+@pytest.fixture
 def clear_hist():
     """ Clears the history in calc_history list """
     Calculator.clear_calc_history()
@@ -27,33 +27,33 @@ def test_calculator_divide_by0():
     """ Tests that Division by Zero catches the error and doesn't crash the program """
     assert Calculator.divide(5, 0) == "DivBy0"
 
-def test_get_last_result():
+def test_get_last_result(clear_hist):
     """ Tests that the function returns the last result in calc_history """
-    clear_hist()
+    #clear_hist()
     assert Calculator.add(5, 4) == 9
     assert Calculator.add(6, 4) == 10
     assert Calculator.get_last_result_in_history() == 10
 
-def test_get_first_result():
+def test_get_first_result(clear_hist):
     """ Tests that the function returns the first result in calc_history """
-    clear_hist()
+    #clear_hist()
     assert Calculator.add(5, 4) == 9
     assert Calculator.add(6, 4) == 10
     assert Calculator.get_first_result_in_history() == 9
 
-def test_count_history():
+def test_count_history(clear_hist):
     """ Tests that the number of calculations in calc_history is correct """
-    clear_hist()
+    #clear_hist()
     assert Calculator.count_history() == 0
     assert Calculator.add(5, 4) == 9
     assert Calculator.add(6, 4) == 10
     assert Calculator.count_history() == 2
 
-def test_get_history():
+def test_get_history(clear_hist):
     """ The return value of get_calc_history() is a list of the calculation results from history"""
     # This tests that the returned list has the calculation history results
     # in order from oldest to newest
-    clear_hist()
+    #clear_hist()
     #get_calc_history
     assert Calculator.count_history() == 0
     assert Calculator.add(5, 4) == 9
@@ -64,9 +64,9 @@ def test_get_history():
     assert  hist_list[0] == 9
     assert hist_list[1] == 10
 
-def test_clear_hist():
+def test_clear_hist(clear_hist):
     """ Tests that calc_history list is cleared """
-    clear_hist()
+    #clear_hist()
     assert Calculator.add(5, 4) == 9
     assert Calculator.add(6, 4) == 10
     assert Calculator.count_history() == 2
